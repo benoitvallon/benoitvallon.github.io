f3857cb7dfa1ef25796f88021d1426924875bb0f
Benoit VALLON <benoitvallon@gmail.com>
2016-03-30 00:27:25 -0400

    Adds javascript concatenation

---
+++ js/main.js
@@ -0,0 +1,11 @@
+---
+---
+
+<!-- jQuery -->
+{% include_relative jquery.min.js %}
+
+<!-- Bootstrap Core JavaScript -->
+{% include_relative javascripts/bootstrap.min.js %}
+
+<!-- Custom Theme JavaScript -->
+{% include_relative custom.js %}
